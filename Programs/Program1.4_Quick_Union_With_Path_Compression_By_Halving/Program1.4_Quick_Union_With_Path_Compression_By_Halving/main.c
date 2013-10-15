717a19f015c380be8e58e5363510081580934831
Mingming Wang <mingming.wang@innocellence.com>
2013-10-15 10:07:15 +0800

    Add Program 1.4 Quick Union with Path Compression by Halving

---
+++ Programs/Program1.4_Quick_Union_With_Path_Compression_By_Halving/Program1.4_Quick_Union_With_Path_Compression_By_Halving/main.c
@@ -0,0 +1,53 @@
+//
+//  main.c
+//  Program1.3_Weighted_Quick_Union
+//
+//  Created by Mingming Wang on 10/15/13.
+//  Copyright (c) 2013 NA. All rights reserved.
+//
+
+#include <stdio.h>
+#define N 100
+int main(int argc, const char * argv[])
+{
+    int i,j, p, q, id[N], sz[N];
+    
+    for (i = 0; i < N; i++) {
+        id[i] = i;
+        sz[i] = 1;
+    }
+    
+    printf("Please input two numders seperated by '-':\n");
+    while (scanf("%d-%d", &p, &q) == 2) {
+        
+        // While traversing the tree through a path,
+        // we make the bottom's root as the same as
+        // the top one's.
+        for (i = p; id[i] != i; i = id[i]){
+            id[i] = id[id[i]];
+        }
+        for (j = q; id[j] != j; j = id[j]){
+            id[j] = id[id[j]];
+        }
+        
+        if (i == j) {
+            printf("Exsiting Pair.\n");
+            continue;
+        }
+        
+        if (sz[i] < sz[j]) {
+            
+            id[i] = j;
+            sz[j] += sz[i];
+            
+        }else{
+            
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        printf("New Pair: %d-%d\n", p, q);
+    }
+    
+    return 0;
+}
+
